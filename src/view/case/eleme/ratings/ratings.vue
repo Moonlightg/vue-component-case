f03014a6556faca12f5bdee55d7bba6c47e7bf5d
Moonlightg <g1079832313@163.com>
2017-12-06 11:50:25 +0800

    评价完善

--- src/view/case/eleme/ratings/ratings.vue
+++ src/view/case/eleme/ratings/ratings.vue
@@ -1,22 +1,129 @@
 <template>
-  <div class="ele-box">
-    我是ratings
+  <div class="ele-box ele-ratings">
+    <div ref="ratingsWrapper">
+      <div class="ratings-content">
+        <div class="overview">
+          <div class="overview-left">
+            <h1 class="score">{{seller.score}}</h1>
+            <div class="title">综合评分</div>
+            <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+          </div>
+          <div class="overview-right">
+            <div class="score-wrapper">
+              <span class="title">服务态度</span>
+              <ele-star :size=36 :score="seller.serviceScore"></ele-star>
+              <span class="score">{{seller.serviceScore}}</span>
+            </div>
+            <div class="score-wrapper">
+              <span class="title">商品评分</span>
+              <ele-star :size="36" :score="seller.foodScore"></ele-star>
+              <span class="score">{{seller.foodScore}}</span>
+            </div>
+            <div class="delivery-wrapper">
+              <span class="title">送达时间</span>
+              <span class="delivery">{{seller.deliveryTime}}分钟</span>
+            </div>
+          </div>
+        </div>
+        <page-split></page-split>
+        <ele-rating-select @select="selectRating" @toggle="toggleContent" :selectType="selectType" :onlyContent="onlyContent" :ratings="ratings"></ele-rating-select>
+        <div class="rating-wrapper">
+          <ul>
+            <li v-for="rating in ratings" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
+              <div class="avatar">
+                <img width="28" height="28" :src="rating.avatar">
+              </div>
+              <div class="content">
+                <h1 class="name">{{rating.username}}</h1>
+                <div class="star-wrapper">
+                  <ele-star :size="24" :score="rating.score"></ele-star>
+                  <span class="delivery">{{rating.deliveryTime}}</span>
+                </div>
+                <p class="text">{{rating.text}}</p>
+                <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                  <i class="icon-smile"></i>
+                  <span class="item" v-for="item in rating.recommend">{{item}}</span>
+                </div>
+                <div class="time">
+                  {{rating.rateTime | formatDate}}
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-  },
-  data () {
-    return {
+  import BScroll from 'better-scroll'
+  import {formatDate} from '../common/js/date'
+  import EleStar from '../components/EleStar'
+  import EleRatingSelect from '../components/EleRatingSelect'
+
+  const ALL = 2
+  const ERR_OK = 0
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        ratings: [],
+        selectType: ALL,
+        onlyContent: true
+      }
+    },
+    methods: {
+      needShow (type, text) {
+        if (this.onlyContent && !text) {
+          return false
+        }
+        if (this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType
+        }
+      },
+      selectRating (type) {
+        this.selectType = type
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      },
+      toggleContent () {
+        this.onlyContent = !this.onlyContent
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      }
+    },
+    created () {
+      this.$http.get('./api/ratings').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$refs.ratingsWrapper, {
+              click: true
+            })
+          })
+        }
+      })
+    },
+    filters: {
+      formatDate (time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd hh:mm')
+      }
+    },
+    components: {
+      EleStar,
+      EleRatingSelect
     }
-  },
-  methods: {
-  },
-  created () {
-  },
-  components: {
   }
-}
 </script>
